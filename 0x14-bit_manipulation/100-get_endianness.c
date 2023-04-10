916dce11275b991ab8919cff839dce250df2329c
Daniel-Kav <99611930+Daniel-Kav@users.noreply.github.com>
2023-04-11 00:32:09 +0300

    Update 100-get_endianness.c
    wury

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of var.
- *
- *
+ * get_endianness - Checks the endianness.
  *
  * Return: 0 If big-endian.
  *         1 If little-endian.
  */
 
-int get_endianes(void)
+int get_endianness(void)
 {
 	char *y;
-	int x  = 1;
+	int x = 1;
 
 	y = (char *)&x;
 
 	return (*y);
-	
 }
