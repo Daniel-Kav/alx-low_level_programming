9b099ec2a4e310ae59785c747cd4e456a15d32a0
Daniel-Kav <99611930+Daniel-Kav@users.noreply.github.com>
2023-04-11 00:28:22 +0300

    Update 5-flip_bits.c
    tqsk

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,26 +5,24 @@
  *             flipped to get from one number to another.
  * @n: The number.
  * @m: The number to flip n to.
- 
- 
- *dan coded this.
+ *
  * Return: number of bits to flip to get from n to m.
- 
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-		unsigned long int btw, check;
-		unsigned int num, i;
+	unsigned long int btw, check;
+	unsigned int num, i;
 
-		check = 1;
-		bet = n ^ m;
-		num = 0;
+	check = 1;
+	btw = n ^ m;
+	num = 0;
 
-		for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
-		{
-			if (check == (bet & check))
-				num++;
-				check <<= 1;
-		}													}
-		return (num);
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (check == (btw & check))
+			num++;
+		check <<= 1;
+	}
+	return (num);
 }
